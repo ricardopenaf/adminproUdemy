dc30ca61305d6da4ea37a1b4277ecce6ee3922ac
ricardopenaf <ricardopenaf@gmail.com>
2018-04-06 20:43:43 -0500

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+// Componentes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumsComponent } from './shared/breadcrums/breadcrums.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { RegisterComponent } from './login/register.component';
 
 // Rutas
 import { APP_ROUTES } from './app.route';
-import { PagesComponent } from './pages/pages.component';
-import { RegisterComponent } from './login/register.component';
+
+// Modulos
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumsComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
